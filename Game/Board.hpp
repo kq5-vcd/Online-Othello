5f1e3367e7cd45645b7ba269b7676ffea7bbde78
unknown <duy822@ymail.com>
2021-04-07 11:31:25 +0700

    Board header declaration

--- Game/Board.hpp
+++ Game/Board.hpp
@@ -4,9 +4,23 @@ private:
     int rows = 8; 
     int cols = 8;
     int board[8][8];
+    
+    bool isOnBoard();
+    bool validDirection(int player, int x, int y, int dir_x, int dir_y);
+
+    void setPiece(int player, int x, int y);
+    void initPieces();
+
 public:
     Board();
 
     void newBoard();
-    void setPiece(int player, int x, int y);
+    void setAvailableMoves(int player);
+    void makeMove(int player, int x, int y);
+
+    int* getScores();
+    void printBoard(); 
+
+    bool validateMove(int player, int x, int y);
+    bool playable();
 };
