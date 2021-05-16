2649f1a793f1232db8300bd3538d2048f4d7466f
BysGits <quandh619@gmail.com>
2021-05-16 18:57:54 +0700

    save

--- Game/server.cpp
+++ Game/server.cpp
@@ -263,7 +263,7 @@ void *connection_handler(void *client_socket){
       vector<int> status = it->getGame().getStatus();
       stringstream result;
       copy(status.begin(), status.end(), ostream_iterator<int>(result, " "));
-      string mess;
+      string mess = "";
       if(it->getPlayers().size() == 1) {
         printf("%s\n",result.str().c_str());
         send(socket, mess.c_str(),mess.length(),0);
@@ -280,14 +280,20 @@ void *connection_handler(void *client_socket){
         mess.append(turn).append(" ").append(it->getPlayers()[0].getName()).append(" ").append(it->getPlayers()[1].getName()).append(" ").append(score1).append(" ").append(score2);
         if(turn.compare("1") == 0){
           send(it->getPlayers()[0].getSocket(), mess.c_str(),mess.length(),0);
-          string m = mess.substr(0,64);
+          string m = "";
+          for(int i = 0; i<64; i++){
+            m.append(info[i]).append(" ");
+          }
           replaceAll(m,"-1","0");
           m.append(turn).append(" ").append(it->getPlayers()[0].getName()).append(" ").append(it->getPlayers()[1].getName()).append(" ").append(score1).append(" ").append(score2);
           send(it->getPlayers()[1].getSocket(), m.c_str(),m.length(),0);
         }
         else if(turn.compare("2") == 0){
           send(it->getPlayers()[1].getSocket(), mess.c_str(),mess.length(),0);
-          string m = mess.substr(0,64);
+          string m = "";
+          for(int i = 0; i<64; i++){
+            m.append(info[i]).append(" ");
+          }
           replaceAll(m,"-1","0");
           m.append(turn).append(" ").append(it->getPlayers()[0].getName()).append(" ").append(it->getPlayers()[1].getName()).append(" ").append(score1).append(" ").append(score2);
           send(it->getPlayers()[0].getSocket(), m.c_str(),m.length(),0);
